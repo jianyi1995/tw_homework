08c4f939f6139e9bb37dad3b6ebecf6d1bd04dce
yudengqing <yiruowuhen@gmail.com>
2016-07-20 17:42:56 +0800

    完成不考虑优惠商品时的小票打印功能

--- cash_register/CashRegister.py
+++ cash_register/CashRegister.py
@@ -27,7 +27,7 @@ class CashRegister():
             return good_information_list
 
     def produce_shopping_good_list(self):
-        tmp_dict = {}  # 存放商品编码和数量
+        tmp_dict = {}
         with open('../input.json', 'r+', encoding='utf-8') as f:
             data = json.load(f)
             for item in data:
@@ -36,7 +36,44 @@ class CashRegister():
                     tmp_dict[tmp[0]] = float(tmp[1])
                     data.remove(item)
             count_dict = Counter(data)
-            shopping_dict = dict(count_dict, **tmp_dict)
+            shopping_dict = dict(count_dict, **tmp_dict) # 存放商品编码和购买数量
             return shopping_dict
+
+    def print_ticket(self):
+        shopping_dict = self.produce_shopping_good_list()
+        total_cost = 0.0
+        name = ''
+        unit = ''
+        price = 0.0
+        single_total = 0.0
+        for key in shopping_dict:
+            result = self.isRightBarcode(key)
+            if not result:
+                msg = key + '不是一个正确的条形码,请检查'
+                print(msg)
+                return
+
+        print('***<没钱赚商店>购物清单***')
+        for key in shopping_dict:
+            count = shopping_dict[key]
+            good_list = self.produce_good_information_list()
+            for item in good_list:
+                if item['barcode'] == key:
+                    name = item['name']
+                    unit = item['unit']
+                    price = item['price']
+
+            single_total = price * count
+            total_cost += single_total
+            print('\n')
+            single_info = '名称: ' + name + ',' + ' 数量: ' + str(count) + '(' + unit + ')' + ',' + ' 单价: ' + str(price) + '(元)' + ',' + ' 小计: ' + str(single_total) + '(元)'
+            print(single_info)
+
+        print('\n')
+        print('----------------------')
+        print('\n')
+        print('总计: ' + str(total_cost) + '(元)')
+        print('\n')
+        print('**********************')
 
 
