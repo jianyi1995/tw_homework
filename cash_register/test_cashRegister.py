73cedf0e792c958fc0f1af6623a6240a550542fa
shaowenhan <1138500211@qq.com>
2016-07-20 21:38:01 +0800

    add testcase about printing without discount with only 1 good with only barcood

--- cash_register/test_cashRegister.py
+++ cash_register/test_cashRegister.py
@@ -1,10 +1,9 @@
 from unittest import TestCase
 from cash_register.CashRegister import CashRegister
 
+
 class TestCashRegister(TestCase):
-
     def test_legal_barcode(self):
-
         cash_register = CashRegister()
         self.assertEqual(cash_register.isRightBarcode('ITEM000001'), True)
         self.assertEqual(cash_register.isRightBarcode('ITEM000002-2'), True)
@@ -25,4 +24,15 @@ class TestCashRegister(TestCase):
 
     def test_shopping_good_list(self):
         cash_register = CashRegister()
-        self.assertIsNotNone(cash_register.produce_shopping_good_list())
+        self.assertIsNotNone(cash_register.produce_shopping_good_list('../input.json'))
+
+    def test_print_without_discount(self):
+        cash_register = CashRegister()
+        expected_out = \
+            '***<没钱赚商店>购物清单***\n' + \
+            '名称: 可口可乐, 数量: 2(瓶), 单价: 3.0(元), 小计: 6.0(元)\n' + \
+            '----------------------\n' + \
+            '总计: 6.0(元)\n\n' + \
+            '**********************'
+
+        self.assertEqual(expected_out, cash_register.print_ticket('../no_discount_with_1_good_with_only_barcood.json'))
