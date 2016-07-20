26a85544a9501695100f09980f6b9dce8849d5d7
shaowenhan <1138500211@qq.com>
2016-07-20 21:48:33 +0800

    add more testcase about no discount situation.

--- cash_register/test_cashRegister.py
+++ cash_register/test_cashRegister.py
@@ -44,4 +44,13 @@ class TestCashRegister(TestCase):
             '**********************'
         self.assertEqual(expected_out, cash_register.print_ticket('../no_discount_with_1_good_with_barcood_with_number.json'))
 
+        expected_out = \
+            '***<没钱赚商店>购物清单***\n' + \
+            '名称: 可口可乐, 数量: 2(瓶), 单价: 3.0(元), 小计: 6.0(元)\n' + \
+            '名称: 苹果, 数量: 5(斤), 单价: 5.5(元), 小计: 27.5(元)\n' + \
+            '----------------------\n' + \
+            '总计: 33.5(元)\n' + \
+            '**********************'
+        self.assertEqual(expected_out, cash_register.print_ticket('../no_discount.json'))
+
 
